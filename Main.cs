60b8149de53edde3dfe141966116f70629380855
Sean Carter <seans206email@gmail.com>
2019-12-10 22:27:04 -0800

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BrokenFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List1 list1 = new List1();
+            list1.ListOfNums(30, 7);
+        }
+    }
+}
